4aac089bc4b7a02c6c7787b65e80f08000929d44
Joselin Espinosa <joss_1204@ciencias.unam.mx>
2025-03-29 22:36:40 -0600

    df

--- Proyecto.py
+++ Proyecto.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import streamlit as st
+
+st.title("¡Hola, Streamlit desde VS Code! 🎈")
+st.write("Esta es una prueba de Streamlit.")
+
 alumnos = pd.Series( ['Mau', 'Mayte', 'Jair', 'Francisco'] )
 calificaciones = pd.Series([10.15, 10.9, 10.0, 10.21])
 
