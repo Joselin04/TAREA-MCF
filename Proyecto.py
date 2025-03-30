41a6d9fd0b3d39fcf6fa0ac12debea2bae06a4a0
Joselin Espinosa <joss_1204@ciencias.unam.mx>
2025-03-29 22:33:47 -0600

    df

--- Proyecto.py
+++ Proyecto.py
@@ -1,2 +1,8 @@
+import pandas as pd
+alumnos = pd.Series( ['Mau', 'Mayte', 'Jair', 'Francisco'] )
+calificaciones = pd.Series([10.15, 10.9, 10.0, 10.21])
 
+tabla = pd.DataFrame( { 'Alumno': alumnos, 'Calificacion': calificaciones } )
+
+print (tabla)
 
