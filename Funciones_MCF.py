f07cc83889836964a0b7df04ab76f17dbfe10feb
Joselin Espinosa <joss_1204@ciencias.unam.mx>
2025-04-01 17:57:52 -0600

    ok

--- Funciones_MCF.py
+++ Funciones_MCF.py
@@ -6,24 +6,31 @@ import matplotlib.pyplot as plt
 import scipy.stats as stats
 from scipy.stats import kurtosis, skew, shapiro, norm
 
+#Funcion para obtener los datos historicos de nuestro activo, en este caso BIMBO, desde 01 de enero de 2010 hasta la fecha del dia de hoy.
 def obtener_datos(accion):
     df = yf.download(accion, start="2010-01-01")['Close']
     return df
 
+#Funcion para calcular nuestros rendimientos diarios a partir de los datos hsitoricos.
 def calcular_rendimientos(df):
     return df.pct_change().dropna()
 
-'''def VaR_normal(media, stdv, df_rend):
-    alphas = [0.95, 0.975, 0.99]
-    VaR_n = []
-    for a in alphas:
-        #VaR parametrico
-        VaR_p = (norm.ppf(1-a, media, stdv))
-        #VaR historico
-        VaR_h = (df_rend.quantile(1-a))
-        #VaR Monte Carlo
-        simulaciones = 100000
-        sim_returns_n = np.random.normal(media,stdv,simulaciones)
-        VaR_MC = np.percentile(sim_returns_n,(1-a)*100)
-        VaR_n.append((VaR_p, VaR_h, VaR_MC))
-    return (VaR_n)'''
+#Funcion para calcular el VaR parametrico de una Normal y una t-student 
+def VaR_parametrico(a, media, stdv, grd_lib):
+    pVaR_n = (norm.ppf(1-a, media, stdv,))
+    pVaR_t = (stats.t.ppf(1-a, grd_lib, media,stdv))
+    return pVaR_n, pVaR_t
+
+def VaR_MonteCarlo (a, media, stdv, grd_lib):
+    simulaciones = 100000
+    sim_returns_n = np.random.normal(media, stdv, simulaciones)
+    sim_returns_t = stats.t.rvs(grd_lib, media, stdv, simulaciones)
+
+    MCVaR_n = np.percentile(sim_returns_n,(1-a)*100)
+    MCVaR_t = np.percentile(sim_returns_t,(1-a)*100)
+    return MCVaR_n, MCVaR_t
+
+def VaR_historico (df, a):
+    hVaR = (df.quantile(1-a))
+    return hVaR
+
--- Proyecto.py
+++ Proyecto.py
@@ -22,53 +22,57 @@ with col2:
 df_bimbo_precios = MCF.obtener_datos("BIMBOA.MX")
 df_bimbo_rend = MCF.calcular_rendimientos(df_bimbo_precios)
 
+#Calculamos la media, curtosis, sesgo y desviacion estandar de los rendimientos diarios de nuestro activo. 
 media_bimbo = np.mean(df_bimbo_rend)
+media =  float(media_bimbo)
+
 curtosis_bimbo = kurtosis(df_bimbo_rend)
+curtosis = float(curtosis_bimbo.item())
+
 sesgo_bimbo = skew(df_bimbo_rend)
-desv_std_bimbo = np.std(df_bimbo_rend) 
+sesgo =  float(sesgo_bimbo.item())
+
+desv_std_bimbo = np.std(df_bimbo_rend)
+stdv = float(desv_std_bimbo.iloc[0])
+
+#Calculamos los grados de libertad para usar una distribucion t-student en nuestros redimientos diarios.
 grados_lib_bimbo = len(df_bimbo_rend)-1
 
-alphas = [0.95, 0.975, 0.99]
 
 '''st.metric("Rendimiento Medio Diario", f"{media_bimbo:.4%}")
 st.metric("Desviación Estándar", f"{desv_std_bimbo:.4f}")
 st.metric("Curtosis", f"{curtosis_bimbo:.4f}")
 st.metric("Sesgo", f"{sesgo_bimbo:.4f}")'''
 
+alphas = [0.95, 0.975, 0.99]
 
-#for a in alphas:
- #   VaR_n = (norm.ppf(1-a, media_bimbo,desv_std_bimbo))
-  #  print (VaR_n)
+VaR_parametricos = {}
+for p in alphas:
+    pVaR_n, pVaR_t = MCF.VaR_parametrico(p, media, stdv, grados_lib_bimbo)
+    VaR_parametricos[p] = {"VaR Normal": pVaR_n, "VaR t-Student": pVaR_t}
 
-#VaR parametrico para α = 0.95, 0.975, y 0.99, distribucion normal
-VaR_n_95 = (norm.ppf(1-0.95, media_bimbo,desv_std_bimbo))
-VaR_n_975 = (norm.ppf(1-0.975, media_bimbo,desv_std_bimbo))
-VaR_n_99 = (norm.ppf(1-0.99, media_bimbo,desv_std_bimbo))
+print("\nVaR bajo una aproximacion parametrica: ")
+for alpha, valores in VaR_parametricos.items():
+    print(f"Alpha: {alpha} | VaR Normal: {pVaR_n:.6f} | VaR t-Student: {pVaR_t:.6f}")
 
-#VaR parametrico para α = 0.95, 0.975, y 0.99, distribucion t-student
-VaR_t_95 = (stats.t.ppf(1-0.95, grados_lib_bimbo, media_bimbo, desv_std_bimbo))
-VaR_t_975 = (stats.t.ppf(1-0.975, grados_lib_bimbo, media_bimbo, desv_std_bimbo))
-VaR_t_99 = (stats.t.ppf(1-0.99, grados_lib_bimbo, media_bimbo, desv_std_bimbo))
+VaR_MonteCarlo = {}
+for m in alphas:
+    MCVaR_n, MCVaR_t = MCF.VaR_MonteCarlo(m, media, stdv, grados_lib_bimbo)
+    VaR_MonteCarlo[m] = {"VaR Normal": MCVaR_n, "VaR t-Student": MCVaR_t}
 
-#VaR historico para α = 0.95, 0.975, y 0.99
-hVaR_n_95 = (df_bimbo_rend.quantile(0.05))
-hVaR_n_975 = (df_bimbo_rend.quantile(0.025))
-hVaR_n_99 = (df_bimbo_rend.quantile(0.01))
+print("\nVaR bajo una aproximacion Monte Carlo: ")
+for alpha, valores in VaR_MonteCarlo.items():
+    print(f"Alpha: {alpha} | VaR Normal: {MCVaR_n:.6f} | VaR t-Student: {MCVaR_t:.6f}")
 
-#VaR MonteCarlo para α = 0.95, 0.975, y 0.99 
-simulaciones = 100000
-sim_returns_n = np.random.normal(media_bimbo,desv_std_bimbo,simulaciones)
-sim_returns_t = stats.t.rvs(grados_lib_bimbo, media_bimbo,desv_std_bimbo,simulaciones)
+VaR_historicos = {}
+for h in alphas:
+    hVaR = MCF.VaR_historico(df_bimbo_rend, h)
+    VaR_historicos[h] = {"VaR historico": hVaR}
 
-#VaR MonteCarlo para distribucion Normal
-MCVaR_n_95 = np.percentile(sim_returns_n,5)
-MCVaR_n_975 = np.percentile(sim_returns_n,2.5)
-MCVaR_n_99 = np.percentile(sim_returns_n,1)
-
-#VaR MonteCarlo para distribucion t-student
-MCVaR_t_95 = np.percentile(sim_returns_t,5)
-MCVaR_t_975 = np.percentile(sim_returns_t,2.5)
-MCVaR_t_99 = np.percentile(sim_returns_t,1)
+print("\nVaR bajo una aproximacion historica: ")
+for alpha, valores in VaR_historicos.items():
+    hVaR = float(valores["VaR historico"].iloc[0])
+    print(f"Alpha: {alpha} | VaR historico: {hVaR:.6f}")
 
 #CVaR historico para una dist normal
 CVaR_95_his = np.mean(df_bimbo_rend[df_bimbo_rend <= VaR_n_95])
@@ -79,7 +83,6 @@ CVaR_95_par = np.mean(df_bimbo_rend[df_bimbo_rend <= hVaR_n_95])
 CVaR_99_par = np.mean(df_bimbo_rend[df_bimbo_rend <= hVaR_n_99])
 
 #Rolling Window
-
 rolling_mean = df_bimbo_rend.rolling(window=252).mean()
 rolling_std = df_bimbo_rend.rolling(window=252).std()
 
@@ -89,18 +92,13 @@ VaR_95_rolling_percent = (VaR_95_rolling * 100).round(4)
 vaR_rolling_df = pd.DataFrame({'Date': df_bimbo_rend.index, '95% VaR Rolling': VaR_95_rolling_percent.squeeze()})
 vaR_rolling_df.set_index('Date', inplace=True)
 
-#plt.figure(figsize=(14, 7))
-#plt.plot(df_bimbo_rend.index, df_bimbo_rend * 100, label='Daily Returns (%)', color='blue', alpha=0.5)
-#plt.plot(vaR_rolling_df.index, vaR_rolling_df['95% VaR Rolling'], label='95% Rolling VaR', color='red')
-#plt.title('Daily Returns and 95% Rolling VaR')
-#plt.xlabel('Date')
-#plt.ylabel('Values (%)')
-#plt.legend()
-#plt.tight_layout()
-#plt.show()
-
-valores = vaR_rolling_df.iloc[[250,251,252,253,254,255]]
-print(valores)
-#print(vaR_rolling_df.tail())
-print(desv_std_bimbo)
+plt.figure(figsize=(14, 7))
+plt.plot(df_bimbo_rend.index, df_bimbo_rend * 100, label='Daily Returns (%)', color='blue', alpha=0.5)
+plt.plot(vaR_rolling_df.index, vaR_rolling_df['95% VaR Rolling'], label='95% Rolling VaR', color='red')
+plt.title('Daily Returns and 95% Rolling VaR')
+plt.xlabel('Date')
+plt.ylabel('Values (%)')
+plt.legend()
+plt.tight_layout()
+plt.show()
 
